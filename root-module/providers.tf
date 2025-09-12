4605c373b1c5f240ab76d39bd6c99a070a67e451
mars <shaibekovmb@gmail.com>
2025-09-11 21:06:06 -0500

    removed bucket comment

--- root-module/providers.tf
+++ root-module/providers.tf
@@ -7,7 +7,6 @@ terraform {
   }
 
   backend "s3" {
-    # bucket       = "adapt-ex-tfstatebucket-staging"
     key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
