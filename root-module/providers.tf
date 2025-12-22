631a63f332ab4b7d29bd856353113cbcc0bc4f81
sergii chreevko <sergii.cherevko.github@gmail.com>
2025-12-21 23:57:19 -0600

    distroy infra

--- root-module/providers.tf
+++ root-module/providers.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket       = "fp-practice-tfstatebucket-dev"
+    # bucket       = "fp-practice-tfstatebucket-dev"
     key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
