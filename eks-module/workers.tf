7d5e2b76647744c6883a3a8883f6e95310c0c1c2
sergii chreevko <sergii.cherevko.github@gmail.com>
2025-12-28 18:10:34 -0600

    create infra

--- eks-module/workers.tf
+++ eks-module/workers.tf
@@ -49,7 +49,7 @@ resource "aws_iam_role_policy_attachment" "workers-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.worker_nodes_role.name
 }
-
+ 
 resource "aws_iam_role_policy_attachment" "workers-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.worker_nodes_role.name
